aab27542e77e633345e34a6fba80c4828b6a8ae8
QuocTrieu <21522714@gm.uit.edu.vn>
2024-12-13 00:00:39 +0700

    finish Chapter 12: Module Product

--- src/main/java/vn/hoidanit/laptopshop/controller/client/HomePageController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/HomePageController.java
@@ -4,11 +4,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import vn.hoidanit.laptopshop.service.ProductService;
+
 @Controller
 public class HomePageController {
 
+  private final ProductService productService;
+
+  public HomePageController(ProductService productService) {
+    this.productService = productService;
+  }
+
   @GetMapping("/")
   public String getHomePage(Model model) {
+
+    model.addAttribute("products", productService.getAll());
+
     return "client/homepage/show";
   }
 }
